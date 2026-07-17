0fc587d219c1ce92b31715d86d6101400fdd905d
tyler <tylercusack@gmail.com>
2026-07-16 23:21:42 -0400

    extendo 1.1.1: add verified url param

--- Casks/extendo.rb
+++ Casks/extendo.rb
@@ -2,7 +2,8 @@ cask "extendo" do
   version "1.1.1"
   sha256 "3bb32b5d2681ba46767173867b654d711b191c0e2b3216a83119718977ab39dd"
 
-  url "https://github.com/tjq/homebrew-tap/releases/download/v#{version}/extendo-#{version}.zip"
+  url "https://github.com/tjq/homebrew-tap/releases/download/v#{version}/extendo-#{version}.zip",
+      verified: "github.com/tjq/homebrew-tap/"
   name "extendo"
   desc "Menu bar clipboard history manager with encrypted iCloud sync"
   homepage "https://extendo.poob.me"
